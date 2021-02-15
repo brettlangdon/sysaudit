3a5d035a0b8edc7b3be2c82d21274aa0a969c286
brettlangdon <me@brett.is>
2021-02-15 08:09:10 -0500

    fix type hinting and checking

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,10 +3,10 @@ import sys
 import sysaudit
 
 
-def test_module():
+def test_module():  # type: () -> None
     if sys.version_info >= (3, 8, 0):
-        assert sysaudit.audit == sys.audit
-        assert sysaudit.addaudithook == sys.addaudithook
+        assert sysaudit.audit == sys.audit  # type: ignore [attr-defined]
+        assert sysaudit.addaudithook == sys.addaudithook  # type: ignore [attr-defined]
     else:
         assert sysaudit.audit == sysaudit._csysaudit.audit
         assert sysaudit.addaudithook == sysaudit._csysaudit.addaudithook
