3862a1f349a939805b2b78ebdd7d7f4e3e45b5cb
Brett Langdon <brett@blangdon.com>
2021-02-15 21:25:17 -0500

    Ensure all implementations get tested (#10)
    * Ensure all implementations get tested
    
    * fix py2.7 syntax
    
    * fix typing
    
    * Update sysaudit/__init__.py

--- sysaudit/__init__.pyi
+++ sysaudit/__init__.pyi
@@ -4,3 +4,15 @@ def audit(event: str, *args: typing.Any) -> None: ...
 def addaudithook(
     hook: typing.Callable[[str, typing.Tuple[typing.Any, ...]], None]
 ) -> None: ...
+
+_audit_fn = typing.Callable[[str, typing.Any], None]
+_addaudithook_fn = typing.Callable[
+    [typing.Callable[[str, typing.Tuple[typing.Any, ...]], None]], None
+]
+
+std_audit = typing.Optional[_audit_fn]
+std_addaudithook = typing.Optional[_addaudithook_fn]
+csysaudit_audit = typing.Optional[_audit_fn]
+csysaudit_addaudithook = typing.Optional[_addaudithook_fn]
+py_audit = typing.Optional[_audit_fn]
+py_addaudithook = typing.Optional[_addaudithook_fn]
