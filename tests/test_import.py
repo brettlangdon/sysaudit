b3fc2cbb0269125d74f50af16b7d77c5966e6231
brettlangdon <me@brett.is>
2021-02-15 21:11:30 -0500

    fix typing

--- tests/test_import.py
+++ tests/test_import.py
@@ -8,8 +8,8 @@ def test_module():  # type: () -> None
     assert sysaudit.addaudithook is not None
 
     if sys.version_info >= (3, 8, 0):
-        assert sysaudit.std_audit == sys.audit
-        assert sysaudit.std_addaudithook == sys.addaudithook
+        assert sysaudit.std_audit == sys.audit  # type: ignore
+        assert sysaudit.std_addaudithook == sys.addaudithook  # type: ignore
         assert sysaudit.audit == sys.audit  # type: ignore [attr-defined]
         assert sysaudit.addaudithook == sys.addaudithook  # type: ignore [attr-defined]
     else:
